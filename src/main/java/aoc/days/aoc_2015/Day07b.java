b6132f10b3e0b89698bcb00d6b241a682eb4ef7c
jezinka <jezina1989@gmail.com>
2023-07-09 12:00:31 +0200

    day 7b

---
+++ src/main/java/aoc/days/aoc_2015/Day07b.java
@@ -0,0 +1,17 @@
+package aoc.days.aoc_2015;
+
+import java.util.List;
+
+
+public class Day07b extends Day07a {
+
+    public Day07b() {
+        super();
+        this.getInput().remove("1674 -> b");
+        this.getInput().add("46065 -> b");
+    }
+
+    public Day07b(List<String> input) {
+        super(input);
+    }
+}
