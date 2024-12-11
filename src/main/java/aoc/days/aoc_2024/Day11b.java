83f802f0e8b4dabc351fd371e8df5fd2b3f17869
jezinka <jezina1989@gmail.com>
2024-12-11 07:06:02 +0100

    2024 -> day 11b to fix

---
+++ src/main/java/aoc/days/aoc_2024/Day11b.java
@@ -0,0 +1,16 @@
+package aoc.days.aoc_2024;
+
+public class Day11b extends Day11a {
+
+    private final int numberOfBlinks;
+
+    public Day11b() {
+        super();
+        this.numberOfBlinks = 75;
+    }
+
+    @Override
+    public int getNumberOfBlinks() {
+        return numberOfBlinks;
+    }
+}
